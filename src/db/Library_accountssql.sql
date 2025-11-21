39bd24c872c0a096b72c1b3d371c6c6f92c43166
Nhlayiseko Divine <hlayisekohh7@gmail.com>
2025-11-21 14:19:24 +0200

    Initial commit database add

---
+++ src/db/Library_accountssql.sql
@@ -0,0 +1,7 @@
+-- Librarian
+INSERT INTO USERS (NAME, EMAIL, PASSWORD, ROLE)
+VALUES ('Divine', 'divine@library.com', 'p123', 'LIBRARIAN');
+
+-- Customer
+INSERT INTO USERS (NAME, EMAIL, PASSWORD, ROLE)
+VALUES ('Hlayza', 'hlayza@library.com', 'p123', 'CUSTOMER');
